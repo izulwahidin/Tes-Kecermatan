39036d9377ec78bdfdb135beba3de6be5c1b6459
PC <pc@priv8.eu.org>
2024-05-18 14:55:24 +0800

    hurus hilang

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { onMount } from 'svelte';
     import { writable, get } from 'svelte/store';
 
     const totalSession = 2;
@@ -6,7 +7,37 @@
     const result = writable(false);
     const sessionCountdown = writable(5);
     const countdown = writable(0);
+
+    const randomList = (model = false) => {
+        let rand = 'abcdefghijklmnopqrstuvwxyz'.split('')
+        shuffle(rand)
+        return rand.slice(1).slice(-5);
+    }
+    
+    const last4 = (arr) => {
+        const x = arr
+        shuffle(x)
+        return x.slice(1).slice(-4);
+    }
+
+    const shuffle = (arr) => {
+        let currentIndex = arr.length
+
+        while (currentIndex != 0) {
+            let randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex--
+
+            [arr[currentIndex],arr[randomIndex]] = [
+                arr[randomIndex], arr[currentIndex]
+            ]
+        }
+    }
+    const rand4 = writable([])
+    const rand5 = writable(randomList())
+    
+    // const store_result = writable([])
     let interval;
+
 
     const startApp = () => {
         startCountdown(3, () => {
@@ -16,6 +47,9 @@
     };
 
     const runSession = () => {
+        rand5.set(randomList())
+        rand4.set(last4($rand5))
+
         sessionCountdown.set(5);
         interval = setInterval(() => {
             const currentCountdown = get(sessionCountdown);
@@ -50,13 +84,52 @@
     export function cleanup() {
         clearInterval(interval);
     }
+
+    const options = 'ABCDE'.split('')
+
+    const chooseOption = (id) => {
+        rand4.set(last4($rand5))
+    }
 </script>
 
 {#if $countdown > 0 && !$result}
     <p>Persiapan {$countdown}</p>
 {:else if $session !== 0 && !$result}
-    <p>Durasi: {$sessionCountdown}</p>
-    <p>Sesi: {$session}</p>
+    <div class="grid gap-4 px-2">
+        <div class="">
+            <h1 class="text-5xl font-bold">Sesi {$session}</h1>
+            <p class="text-sm font-bold">Durasi: {$sessionCountdown} detik</p>
+        </div>
+        <div id="soal" class="flex gap-2 items-center">
+            <h2 class="text-xl font-bold">Soal</h2>
+            <ul class="flex gap-3 font-bold text-center">
+                {#each $rand5 as soal, id}
+                    <li class="border-2 px-6 py-2">
+                        <p class="text-4xl">{soal.toUpperCase()}</p>
+                        <small class="text-sm">{options[id]}</small>
+                    </li>
+                {/each}
+            </ul>
+        </div>
+        <div id="pertanyaan" class="flex gap-2 items-center">
+            <h2 class="text-xl font-bold">Pertanyaan</h2>
+            <ul class="flex gap-3 font-bold">
+                {#each $rand4 as pertanyaan}
+                    <li class="border-2 px-3 py-2">{pertanyaan.toUpperCase()}</li>
+                {/each}
+            </ul>
+        </div>
+        <div id="pilihan_ganda" class="flex gap-2 items-center">
+            <h2 class="text-xl font-bold">Jawaban</h2>
+            <ul class="flex gap-3 font-bold">
+            {#each options as option}
+                <button on:click={() => chooseOption(option)}>
+                    <li class="border-2 px-6 py-2" >{option}</li>
+                </button>
+            {/each}
+            </ul>
+        </div>
+    </div>
 {:else if $result}
     <p>Hasil Tes:</p>
 {:else}
